8c524e0ee39ea9402dcf0603000af922f2aa5ee6
syedsofiyan2004 <syed.sofiyan@minfytech.com>
2025-06-21 19:57:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["14.98.177.234/32"] 
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
